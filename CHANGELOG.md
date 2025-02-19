6f1f52e5f163177e45d44ec71235e5b87461306f
PigeonCodesBack <dev.mauro.derosa@gmail.com>
2025-02-19 21:55:13 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# CHANGELOG
+Inspired from [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+
+## [Unreleased]
+### Added
+- Add web deploy ([#8](https://github.com/MechanicalFlower/godot-template/pull/8))
+- Automating Godot updates with renovate ([#12](https://github.com/MechanicalFlower/godot-template/pull/12))
+### Changed
+- Use Justfile as command runner ([#7](https://github.com/MechanicalFlower/godot-template/pull/7))
+### Deprecated
+### Removed
+### Fixed
+### Security
+### Dependencies
+
+[Unreleased]: https://github.com/MechanicalFlower/godot-template/compare/0.1.0...HEAD
---
+++ plug.gd
@@ -0,0 +1,22 @@
+extends "res://addons/gd-plug/plug.gd"
+
+
+func _plugging():
+	plug(
+		"godot-extended-libraries/godot-debug-menu",
+		{"commit": "3211673efc9d1e41f94bbd74705eaed2d2b8bdd7", "renovate-branch": "master"}
+	)
+	plug(
+		"KoBeWi/Godot-Universal-Fade",
+		{"commit": "f091514bba652880f81c5bc8809e0ee4498988ea", "renovate-branch": "master"}
+	)
+
+	# Patched version
+	plug(
+		"florianvazelle/EasyMenus",
+		{"commit": "018ab39001f862abbd6ca424258a9a548589d61c", "renovate-branch": "master"}
+	)
+	plug(
+		"florianvazelle/Log",
+		{"commit": "51878aa55c4169f19b2c000f0e53b77439654829", "renovate-branch": "main"}
+	)
